ba5db8f29b5dc309266f0ba9a80b429c8d6440ef
Callista <kalista5015@gmail.com>
2020-05-30 00:39:29 +0300

    Adds artists db file

---
+++ src/app/artists.ts
@@ -0,0 +1,23 @@
+export const artists = [
+     {
+          portrait: 'https://i.pinimg.com/originals/6c/04/9d/6c049d4af5d3ab8579d6594b7ba36279.jpg',
+          name: 'Salvador Dalí',
+          country: 'Spain',
+     },
+     {
+          portrait: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Pablo_picasso_1.jpg',
+          name: 'Pablo Picasso',
+          country: 'Spain',
+     },
+     {
+          portrait: 'https://i.pinimg.com/originals/79/eb/e3/79ebe344b647426e425f5301564cca52.jpg',
+          name: 'René Magritte',
+          country: 'Belgium',
+     },
+     {
+          portrait: 'https://i.pinimg.com/originals/0f/d4/d2/0fd4d2b01bf82f48db383fa73d25cd3f.jpg',
+          name: 'Joan Miró',
+          country: 'Spain',
+     },
+     
+]
