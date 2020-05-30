4955d25e4c8a73d0f65c005a9002cd0d395b3e34
Callista <kalista5015@gmail.com>
2020-05-30 14:15:05 +0300

    Creates routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IntroComponent } from './intro/intro.component';
+import { ArtistsComponent } from './artists/artists.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: IntroComponent },
+  { path: '#artists', component: ArtistsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
