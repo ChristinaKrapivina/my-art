9d176e1b86f140d1f82119d9da195db9d9e0c455
Callista <kalista5015@gmail.com>
2020-06-01 16:09:51 +0300

    Adds routing for streams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { WorksComponent } from './works/works.component';
+import { StreamsComponent } from './streams/streams.component';
 
 
 const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'artists', component: ArtistsComponent },
   { path: 'works/:worksId', component: WorksComponent },
+  { path: 'streams', component: StreamsComponent },
 ];
 
 @NgModule({
