06e26a14016a5582dbfd3e0b514caf9cf079f58d
Callista <kalista5015@gmail.com>
2020-06-01 17:53:47 +0300

    Adds styles to movement component

--- src/app/movements/movement/movement.component.ts
+++ src/app/movements/movement/movement.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { movements } from '../../movements';
+import { Movement } from 'src/interfaces';
+
 @Component({
   selector: 'app-movement',
   templateUrl: './movement.component.html',
   styleUrls: ['./movement.component.scss']
 })
 export class MovementComponent implements OnInit {
-
+  movements: Movement[] = movements;
   constructor() { }
 
   ngOnInit(): void {
